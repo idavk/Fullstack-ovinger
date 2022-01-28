987ca6e7ccdea4724a1863f09900f81e972297e2
Ida Angell Veglo Klæstad <iaklaest@stud.ntnu.no>
2022-01-28 17:15:57 +0100

    init

---
+++ oving-2/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+   <h1>Kalkulator</h1>
+    <div class="buttons">
+      <button>0</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>+</button>
+      <button>-</button>
+      <button>*</button>
+      <button>/</button>
+      <button>=</button>
+    </div>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
